fc2f006a924354f3ddf35bbf10d94781f8b7eb71
Ryan Maloney <rypmaloney@gmail.com>
2022-04-25 21:41:24 -0400

    Use cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
