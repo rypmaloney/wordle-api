1ab11931280b21ff3474d0e4bca652219c71787c
Ryan Maloney <rypmaloney@gmail.com>
2022-04-25 21:13:09 -0400

    Changed DB uri

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
     connectionString: isProduction
-        ? process.env.DATABASE_URL
+        ? process.env.DATABASE_URI
         : connectionString,
     ssl: isProduction,
 });
